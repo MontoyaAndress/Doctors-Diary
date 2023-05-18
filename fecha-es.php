31f94e4ba94abb0990aa5fa8112961a8386d23f4
MontoyaAndress <89613744+MontoyaAndress@users.noreply.github.com>
2023-05-18 12:36:12 -0600

    Update fecha-es.php

--- fecha-es.php
+++ fecha-es.php
@@ -3,8 +3,7 @@ function fechaEs($fecha) {
   $fecha = substr($fecha, 0, 10);
   $dia = date('l', strtotime($fecha));
   $dias_ES = array("lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo");
-  $dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
-  $nombredia = str_replace($dias_EN, $dias_ES, $dia);
+  $nombredia = str_replace($dias_ES, $dia);
   return $nombredia;
 }
 ?>
