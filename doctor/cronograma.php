c356e1785fa7042e49e948d5d273ec7c27bff99a
RamsesDiaz27 <89627475+RamsesDiaz27@users.noreply.github.com>
2023-05-19 18:14:17 -0600

    Update cronograma.php

--- doctor/cronograma.php
+++ doctor/cronograma.php
@@ -442,8 +442,8 @@
                                              <img src="../img/notfound.svg" width="25%">
                                              
                                              <br>
-                                             <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
-                                             <a class="non-style-link" href="citas.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Appointments &nbsp;</font></button>
+                                             <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">No encontramos nada!</p>
+                                             <a class="non-style-link" href="citas.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Mostrar todas las citas &nbsp;</font></button>
                                              </a>
                                              </center>
                                              <br><br><br><br>
